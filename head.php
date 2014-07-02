75fd67d210083999b334fb1f0d60afc593f2b76d
justinandersun <justinandersun@gmail.com>
2014-07-01 23:29:10 -0400

    Updated CSS

--- head.php
+++ head.php
@@ -11,6 +11,7 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script type="text/javascript" src="script.js"></script>
 	<!-- STYLESHEETS -->
-	<link type="text/css" rel="stylesheet" href="style.css">
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+	<link type="text/css" rel="stylesheet" href="..\css/style.css">
 </head>
 <body>
