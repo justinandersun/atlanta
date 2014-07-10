279cb8ed5560df5524d0ad39a640f98c863521db
smgehrig <smgehrig@indiana.edu>
2014-07-09 23:56:42 -0400

    added menu

---
+++ menu/menu.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+
+$(document).ready(function(){
+	//
+	  $(".flip").click(function(){
+		  var drop = "#p".concat(this.id);
+    $(drop).slideToggle("slow");
+    });
+});
+
+function collapseall(){
+	$(".panel").slideUp("slow");
+}
+
+function openall(){
+	$(".panel").slideDown("slow");
+}
