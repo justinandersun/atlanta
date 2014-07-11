c190c290ef2322719aec3887800a6865e9704ba1
justinandersun <justinandersun@gmail.com>
2014-07-10 23:25:10 -0400

    Fixed merge issue

--- contact/index.php
+++ contact/index.php
@@ -1,20 +1,11 @@
 <?php include('..\head.php'); ?>
 <?php include('..\header.php'); ?>
 
-<<<<<<< HEAD
-	<div id="contact">
-		<h3>CONTACT</h3>
-	    <div id="number">
-	    	Order Ahead: (770) 955-4413
-		</div>
-	</div>
-
-=======
     <section id="contact">
-    <h3>CONTACT</h3>
-        <div id="number">
-        <h2>Order Ahead: (770) 955 -4413</h2>
+		<div class="page-content">
+			<h3>Contact</h3>
+        	<p>Order Ahead: (770) 955-4413</p>
         </div>
     </section>
->>>>>>> origin/master
+
 <?php include('../footer.php'); ?>
