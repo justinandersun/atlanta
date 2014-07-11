6af3d2f7c4b5813ea63f5190f91d7894b9fafc6b
justinandersun <justinandersun@gmail.com>
2014-07-10 20:57:13 -0400

    PUSH

--- events/index.php
+++ events/index.php
@@ -1,15 +1,14 @@
 <?php include('..\head.php'); ?>
 <?php include('..\header.php'); ?>
 
-	<h3>EVENTS</h3>
-	<p>Crafting the perfect pizza is not something that everyone has figured out. But we have.
-		Not only do we offer a great pie, but we host great events.</p> 
-
-<p>Texas Hold 'Em</p>
-<p>Trivia Thursdays</p>
-<p>Cold Beer Specials</p>
-<p>Multiple televisions for the Big Game</p>
-
-We are looking forward to hanging out with you!
+	<section id="events">
+		<div class="page-content">
+			<h3>Events</h3>
+			<p class="brag">Crafting the perfect pizza is an accomplishment,</p>
+			<p class="brag">But great events are the pepperoni on top.</p>
+			<p>Stop by for Trivia Thursdays, Texas Hold 'Em, Cold Beer Specials,
+				and more!</p>
+		</div>
+	</section>
 
 <?php include('../footer.php'); ?>
