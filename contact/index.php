bf48fa730f7dcb8739dc568fbc0b31646a8a1003
smgehrig <smgehrig@indiana.edu>
2014-07-10 22:58:13 -0400

    contact
    contact

--- contact/index.php
+++ contact/index.php
@@ -1,8 +1,10 @@
 <?php include('..\head.php'); ?>
 <?php include('..\header.php'); ?>
 
-	<h3>CONTACT</h3>
-    <div id="number">
-    Order Ahead: (770) 955 -4413
-	</div>
+    <section id="contact">
+    <h3>CONTACT</h3>
+        <div id="number">
+        <h2>Order Ahead: (770) 955 -4413</h2>
+        </div>
+    </section>
 <?php include('../footer.php'); ?>
