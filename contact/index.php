e702b6855d52af8aee25562c286925efff06e7e4
justinandersun <justinandersun@gmail.com>
2014-07-10 23:13:41 -0400

    Some changes

--- contact/index.php
+++ contact/index.php
@@ -1,8 +1,11 @@
 <?php include('..\head.php'); ?>
 <?php include('..\header.php'); ?>
 
-	<h3>CONTACT</h3>
-    <div id="number">
-    Order Ahead: (770) 955 -4413
+	<div id="contact">
+		<h3>CONTACT</h3>
+	    <div id="number">
+	    	Order Ahead: (770) 955-4413
+		</div>
 	</div>
+
 <?php include('../footer.php'); ?>
